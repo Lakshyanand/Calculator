5a53327e4a125c30b08573f353f8285a3d55729f
Lakshya Anand <100176346+Lakshyanand@users.noreply.github.com>
2022-05-26 01:01:16 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,38 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+
+let screenValue="";
+
+for(item of buttons)
+{
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log(buttonText); 
+        if(buttonText=='X')
+        {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'C')
+        {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if(buttonText == '=')
+        {
+            screen.value = eval(screenValue)
+        }
+        else if(buttonText == '<-')
+        {
+            console.log('backspace');
+            screenValue = screenValue.slice(0,-1);
+            screen.value = screenValue;
+        }
+        else
+        {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
